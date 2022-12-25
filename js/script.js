a091874132e30bd2a310f7bd8ced186919f8e942
Jānis Šņoriņš <janis.snorins@tet.lv>
2022-12-25 19:50:36 +0200

    Performance and accessibility fix

--- js/script.js
+++ js/script.js
@@ -2,20 +2,33 @@
 const currentYearText = document.querySelector('.current-year');
 currentYearText.innerHTML = String(new Date().getFullYear());
 
+// Sets overflow to hidden so user would not be able to scroll when the mobile navigation is open.
+const setBodyOverflow = () => {
+    if (document.body.style.overflow === 'hidden') {
+        document.body.style.overflow = 'auto';
+    } else {
+        document.body.style.overflow = 'hidden';
+    }
+};
+
 // Opens and closes the mobile navigation.
 const header = document.querySelector('.header');
 const mobileNavButton = document.querySelector('.mobile-nav-button');
 mobileNavButton.addEventListener('click', () => {
     header.classList.toggle('nav-open');
-})
+});
 
 // Closes the mobile navigation (if the current screen is mobile or small tablet), once the link is clicked.
 const navLinks = document.querySelectorAll('.main-nav-link');
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         header.classList.toggle('nav-open');
-    })
-})
+
+        if (mobileNavButton.style.display === 'block') {
+            setBodyOverflow();
+        }
+    });
+});
 
 // Handles the sticky navigation logic.
 const sectionHeroEl = document.querySelector('.section-hero');
@@ -30,6 +43,6 @@ const observer = new IntersectionObserver((entries) => {
 }, {
     root: null,
     threshold: 0,
-    rootMargin: '-80px'
+    rootMargin: '-96px'
 });
 observer.observe(sectionHeroEl);
